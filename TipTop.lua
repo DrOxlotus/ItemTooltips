0b8db3a8680cfafb35688ef54166beb7542abaa4
DrOxlotus <alex.metz1024@gmail.com>
2018-08-24 15:22:21 -0500

    Updated the core code for backup.

--- TipTop.lua
+++ TipTop.lua
@@ -1,18 +1,27 @@
 --------------------------------------------------------------------------------
---                            I T E M   T O O L T I P S                       --
+--                                 T I P T O P                                --
 --------------------------------------------------------------------------------
 --               		Copyright 2018 Alex Metz (Oxlotus-Area 52)              	--
 --------------------------------------------------------------------------------
+print("Me");
 local t = GameTooltip;
 
-local function AddItemIDToTooltip(gt) -- GameTooltip (gt)
-  local itemName, itemLink = gt:GetItem();
-  if not itemName or not itemLink then return; end
+local function AddIDToTooltip(gt) -- GameTooltip (gt)
+  print("B");
+  if (gt:GetItem()) then
+    local itemName, itemLink = gt:GetItem();
+    if not itemName or not itemLink then return; end
 
-  local itemString = select(3, strfind(itemLink, "|H(.+)|h"));
-  local _, itemID, _, _, _, _, _, _, _, _, _, _, _, _ = strsplit(":", itemString)
+    local itemString = select(3, strfind(itemLink, "|H(.+)|h"));
+    local _, itemID, _, _, _, _, _, _, _, _, _, _, _, _ = strsplit(":", itemString)
 
-  gt:AddDoubleLine("Item ID: ", itemID, nil, nil, nil, 0.2, 0.7, 1);
+    gt:AddDoubleLine("Item ID: ", itemID, nil, nil, nil, 0.2, 0.7, 1);
+  elseif (gt:GetSpell()) then
+    local _, _, spellID = gt:GetSpell();
+
+    gt:AddDoubleLine("Spell ID: ", spellID, nil, nil, nil, 0.2, 0.7, 1);
 end
 
-t:HookScript("OnTooltipSetItem", AddItemIDToTooltip);
+print("A");
+
+t:HookScript("OnTooltipSetItem", AddIDToTooltip);
