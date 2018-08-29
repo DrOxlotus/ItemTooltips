9e3a5655619a8b4e3aaf0b74acf51d069439c3d3
DrOxlotus <alex.metz1024@gmail.com>
2018-08-29 00:36:15 -0500

    Tweaked a few things.

--- Core.lua
+++ Core.lua
@@ -3,6 +3,7 @@
 --------------------------------------------------------------------------------
 --               		Copyright 2018 Alex Metz (Oxlotus-Area 52)              	--
 --------------------------------------------------------------------------------
+-- Class Variables
 local creature = TipTop_Creature:new()
 local item = TipTop_Item:new()
 local spell = TipTop_Spell:new()
@@ -16,7 +17,7 @@ ItemRefTooltip:HookScript("OnTooltipSetItem", item.AddItemIDToItemRefTooltip)
 
 -- Spell
 --[[
-Developer Note: Spell's don't have their own reference tooltip; they piggyback on
+Developer Note: Spells don't have their own reference tooltip; they piggyback on
 the reference tooltip for items. Why? No idea...
 ]]--
 GameTooltip:HookScript("OnTooltipSetSpell", spell.AddSpellIDToGameTooltip)
